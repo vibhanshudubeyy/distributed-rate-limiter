73d8cd8be80b0474ca13e67030f313031b35ef12
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2026-02-10 16:27:19 +0530

    fixed main bugs

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,30 @@ mod limiter;
 use axum::{routing::get, Router};
 use limiter::middleware::{rate_limit, SharedBucket};
 use limiter::token_bucket::TokenBucket;
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::Mutex;
 
-async fn main(){
-    let bucket: SharedBucket = Arc::new(Mutex::new(TokenBucket::new(10.0, 5.0)));
+#[tokio::main]
+async fn main() {
+    let bucket: SharedBucket = Arc::new(Mutex::new(
+        TokenBucket::new(10.0, 5.0),
+    ));
 
     let app = Router::new()
-        .route("/",get(|| async { "OK" }))
-        .layer(axum::middleware::from_fn_with_state(bucket.clone(), rate_limit))
+        .route("/", get(|| async { "OK" }))
+        .layer(axum::middleware::from_fn_with_state(
+            bucket.clone(),
+            rate_limit,
+        ))
         .with_state(bucket);
 
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("🚀 Server running on {}", addr);
 
-    print("Server running on http://localhost:3000");
-
-    axum::Server::bind(&"0.0.0.3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    axum::serve(
+        tokio::net::TcpListener::bind(addr).await.unwrap(),
+        app,
+    )
+    .await
+    .unwrap();
 }
